d69ab7dadc7dd8f1cccaa97c2428a2d29c006d2c
NZHyena <80725967+NZHyena@users.noreply.github.com>
2022-02-05 10:41:34 +1300

    Added libraries and files for Unit Testing

---
+++ src/test/UnitTesting.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class UnitTesting {
+    @Test
+    public void testDemoTest(){
+        assertTrue(true);
+    }
+}
