88a3d623dd9f826fbca43c42a554d8ad8c522340
NZHyena <80725967+NZHyena@users.noreply.github.com>
2022-02-06 11:08:58 +1300

    Updates Made to UnitTesting file

--- src/test/UnitTesting.java
+++ src/test/UnitTesting.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import main.Task;
@@ -8,68 +9,140 @@ import main.DatabaseConnection;
 
 public class UnitTesting {
 
-    @Test
-    public void testCreateTask() {
+    static TaskController taskController;
+    static DatabaseConnection dbc;
+
+    @BeforeClass
+    public static void setup(){
 
     }
 
     @Test
+    public void testCreateTask() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
     public void testCreateTask2() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testCreateTask3() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testCreateTask4() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testDeleteTask() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testSelectAllTasks() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testSelectTaskById() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testSelectTasksBetweenDates() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testSelectTasksByDate() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testSelectTasksByName() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testUpdateTaskDate() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testUpdateTaskDetails() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 
     @Test
     public void testUpdateTaskName() {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
 }
