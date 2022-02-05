6b1ee45e911f873b3b21b6c1628ca253cc222b87
NZHyena <80725967+NZHyena@users.noreply.github.com>
2022-02-05 21:41:03 +1300

    Implementation of Unit Testing Imported JUnit and related libraries Created UnitTesting Class

---
+++ src/test/UnitTesting.java
@@ -0,0 +1,75 @@
+package test;
+
+import org.junit.Test;
+
+import main.Task;
+import main.TaskController;
+import main.DatabaseConnection;
+
+public class UnitTesting {
+
+    @Test
+    public void testCreateTask() {
+
+    }
+
+    @Test
+    public void testCreateTask2() {
+
+    }
+
+    @Test
+    public void testCreateTask3() {
+
+    }
+
+    @Test
+    public void testCreateTask4() {
+
+    }
+
+    @Test
+    public void testDeleteTask() {
+
+    }
+
+    @Test
+    public void testSelectAllTasks() {
+
+    }
+
+    @Test
+    public void testSelectTaskById() {
+
+    }
+
+    @Test
+    public void testSelectTasksBetweenDates() {
+
+    }
+
+    @Test
+    public void testSelectTasksByDate() {
+
+    }
+
+    @Test
+    public void testSelectTasksByName() {
+
+    }
+
+    @Test
+    public void testUpdateTaskDate() {
+
+    }
+
+    @Test
+    public void testUpdateTaskDetails() {
+
+    }
+
+    @Test
+    public void testUpdateTaskName() {
+
+    }
+}
