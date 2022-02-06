1a93339985b6eb2700f50a7243e3458ce63163eb
NZHyena <80725967+NZHyena@users.noreply.github.com>
2022-02-06 23:49:49 +1300

    Unit Testing Overhaul Removed:       - UnitTesting.java Created:       - TaskTest.java       - TastControllerTest.Java

---
+++ src/test/TaskControllerTest.java
@@ -0,0 +1,147 @@
+package test;
+
+import org.junit.Test;
+
+public class TaskControllerTest {
+
+/*
+ * Note: All current unit testing is run without code that interacts with the database. This is because of issue relating to getting unit tests to communicate with the database
+ * and getting the database to communicate back. It is known that the Java to database and database to java code is running in main(). Manual testing will be noted in the 
+ * BIT707_AS3_Task1a_DeveloperTesting.xlsx spreadsheet
+ */
+
+    @Test
+    public void testCreateTask() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testCreateTask2() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testCreateTask3() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testCreateTask4() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testDeleteTask() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testDisplayTask() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testEditTask() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testFindTaskById() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testFindTasksByName() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testInitialLoad() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testLoadAllTasks() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testReadSerializable() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testSortTask() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testWriteSerializable() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testGetAllTasks() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
---
+++ src/test/TaskTest.java
@@ -0,0 +1,97 @@
+package test;
+
+import org.junit.Test;
+
+public class TaskTest {
+
+
+    @Test
+    public void testCompareTo() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testGetDate() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testGetDetails() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testGetId() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testGetTaskName() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testSetDate() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testSetDetails() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testSetId() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testSetTaskName() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    public void testToString() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
