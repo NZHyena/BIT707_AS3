486f2f142079a268d7f036e2f8451812460e7fd1
NZHyena <80725967+NZHyena@users.noreply.github.com>
2022-02-07 11:11:17 +1300

    Current TaskController Unit Tests

--- src/test/TaskControllerTest.java
+++ src/test/TaskControllerTest.java
@@ -1,12 +1,17 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import main.Task;
@@ -16,55 +21,101 @@ public class TaskControllerTest {
 
 /*
  * Note: All current unit testing is run without code that interacts with the database. This is because of issue relating to getting unit tests to communicate with the database
- * and getting the database to communicate back. It is known that the Java to database and database to java code is running in main(). Manual testing will be noted in the 
+ * and getting the database to communicate back. It is known that the Java to database code and database to java code is running in main(). Manual testing will be noted in the 
  * BIT707_AS3_Task1a_DeveloperTesting.xlsx spreadsheet
  */
 
     TaskController testControl;
 
-    @Test
-    public void testCreateTask() {
-        // Arrange
-
-        // Act
-
-        // Assert
+    @BeforeClass
+    public void setup(){
+        testControl = new TaskController();
     }
 
     @Test
-    public void testCreateTask2() {
+    public void testCreateTaskBase() {
+
+        // Note: Tested and passes with call to database commented out
+
         // Arrange
+        Task notMethod = new Task();
+        notMethod.setTaskName("Task Testing");
+        notMethod.setId(1);
 
         // Act
+        testControl.CreateTask("Task Testing");
+        
 
         // Assert
+        assertEquals(testControl.getAllTasks().get(0).toString(), notMethod.toString());
     }
 
     @Test
-    public void testCreateTask3() {
+    public void testCreateTaskWithDetails() {
+
+        // Note: Tested and passes with call to database commented out
+
         // Arrange
+        Task notMethod = new Task();
+        notMethod.setTaskName("Task Testing");
+        notMethod.setId(1);
+        notMethod.setDetails("Test Details");
 
         // Act
+        testControl.CreateTask("Task Testing", "Test Details");
+        
 
         // Assert
+        assertEquals(testControl.getAllTasks().get(0).toString(), notMethod.toString());
     }
 
     @Test
-    public void testCreateTask4() {
+    public void testCreateTaskWithDate() throws ParseException {
+
+        // Note: Tested and passes with call to database commented out
+
         // Arrange
+        Task notMethod = new Task();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-DD");
+        Date tempDate = formatter.parse("2022-02-07");
+        notMethod.setDate(tempDate);
+        notMethod.setTaskName("Task Testing");
+        notMethod.setId(1);
 
         // Act
-
+        testControl.CreateTask("Task Testing", tempDate);
+        
         // Assert
+        assertEquals(testControl.getAllTasks().get(0).toString(), notMethod.toString());
+    }
+
+    @Test
+    public void testCreateTask4() throws ParseException {
+
+        // Note: Tested and passes with call to database commented out
+
+        // Arrange
+        Task notMethod = new Task();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-DD");
+        Date tempDate = formatter.parse("2022-02-07");
+        notMethod.setDate(tempDate);
+        notMethod.setTaskName("Task Testing");
+        notMethod.setId(1);
+        notMethod.setDetails("Test Details");
+
+        // Act
+        testControl.CreateTask("Task Testing", "Test Details", tempDate);
+        
+        // Assert
+        assertEquals(testControl.getAllTasks().get(0).toString(), notMethod.toString());
     }
 
     @Test
     public void testDeleteTask() {
 
-        // TODO: test without db connection
+        // Note: Tested and passes with call to database commented out
 
         // Arrange
-        testControl = new TaskController();
         Task test = new Task("Test");
         testControl.getAllTasks().add(test);
 
@@ -77,41 +128,41 @@ public class TaskControllerTest {
         assertNotEquals(initalVal, newVal);
     }
 
-    @Test
-    public void testDisplayTask() {
-        // Arrange
-
-        // Act
-
-        // Assert
-    }
+    // Method DisplayTask has been used successfully in all CreateTest Unit Testing
 
     @Test
     public void testEditTask() {
+
+        // Note: Tested and passes with call to database commented out
+
         // Arrange
+        testControl.CreateTask("Incorrect Name");
+        Task toEdit = testControl.getAllTasks().get(0);
+        String initalVal = toEdit.toString();
 
         // Act
+        testControl.EditTask(1, "Correct Name", "", "");
+        Task edited = testControl.getAllTasks().get(0);
+        String newVal = edited.toString();
 
         // Assert
+        assertNotEquals(initalVal, newVal);
     }
 
     @Test
     public void testFindTaskById() {
         // Arrange
+        testControl.CreateTask("FindMe");
 
         // Act
+        Task found = testControl.FindTaskById(1);
 
         // Assert
+        assertTrue(found != null);
+        assertTrue(found.toString().length() > 0);
     }
 
-    @Test
-    public void testFindTasksByName() {
-        // Arrange
-
-        // Act
-
-        // Assert
-    }
+    // Manual Testing done for method FindTasksByName
 
     // Manual Testing done for method LoadAllTasks
 
@@ -168,6 +219,8 @@ public class TaskControllerTest {
     
     @Test
     public void testWriteSerializable() throws InterruptedException {
+
+        // This previous passed
 
         // Arrange
         String filePath = "./tmp/TaskSingleton.ser";
