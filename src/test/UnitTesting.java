20bb4d6c45c0f4def4d1b2bb47c8b2e34f9b8798
NZHyena <80725967+NZHyena@users.noreply.github.com>
2022-02-06 13:25:08 +1300

    Changes to How UnitTesting will be run Note: It won't be, see comment in UnitTesting.java

--- src/test/UnitTesting.java
+++ src/test/UnitTesting.java
@@ -1,8 +1,15 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import main.TaskSingleton;
 import main.Task;
 import main.TaskController;
 import main.DatabaseConnection;
@@ -11,132 +18,125 @@ public class UnitTesting {
 
     static TaskController taskController;
     static DatabaseConnection dbc;
+    Task tmp;
 
-    @BeforeClass
-    public static void setup(){
-
-    }
-
-    // Controller Tests
-    @Test
-    public void testControllerCreateTaskBase() {
-        
-    }
-
-    @Test
-    public void testControllerCreateTaskDetailsArg() {
-        
-    }
-
-    @Test
-    public void testControllerCreateTaskDateArg() {
-        
-    }
-
-    @Test
-    public void testControllerCreateTaskAllArgs() {
-        
-    }
-
-    @Test
-    public void testControllerDeleteTask() {
-        
-    }
-
-    @Test
-    public void testControllerDisplayTask() {
-        
-    }
-
-    @Test
-    public void testControllerEditTask() {
-        
-    }
-
-    @Test
-    public void testControllerFindTaskById() {
-        
-    }
-
-    @Test
-    public void testControllerFindTasksByName() {
-        
-    }
-
-    @Test
-    public void testControllerSortTask() {
-        
-    }
-
-    @Test
-    public void testControllerGetAllTasks() {
-        
-    }
+/*
+ * Note: Attempts have been made to try and get some form of UnitTesting within the code running. Unfortunately this is failing to work.
+ * All future testing will refer to the test plan later in the programming/development cycle.
+ */
 
     // Database Connection Tests
     @Test
-    public void testDBCCreateTaskBase() {
-        
+    public void testDBCCreateTaskBase() throws SQLException {
+
     }
 
     @Test
     public void testDBCDBCCreateTaskDetailsArg() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCCreateTaskDateArg() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCCreateTaskAllArgs() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCDeleteTask() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCSelectAllTasks() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCSelectTaskById() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCSelectTasksBetweenDates() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCSelectTasksByDate() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCSelectTasksByName() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCUpdateTaskDate() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCUpdateTaskDetails() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
     @Test
     public void testDBCUpdateTaskName() {
-        
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 
 }
